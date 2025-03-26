a33457ccb1f3a6509905121deea8ee3008b3625b
SamarthTumdi <samarth.tumdi@gmail.com>
2025-03-26 16:47:06 +0530

    Chip complete

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero"
 import Highlights2 from "./components/Highlights2"
 import Model from "./components/Model"
 import Features from "./components/Features"
+import Chip from "./components/Chip"
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       {/* <Highlights2 /> */}
       {/* <Model /> */}
       <Features />
+      <Chip />
     </main>
   )
 }
