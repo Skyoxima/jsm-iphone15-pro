d5f39f6f3d13cd962c7c558dfb89ce38e4151cb9
SamarthTumdi <samarth.tumdi@gmail.com>
2025-03-27 02:30:53 +0530

    Vercel build fix Try 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import Highlights2 from "./components/Highlights2"
-import Model from "./components/Model"
-import Features from "./components/Features"
-import Chip from "./components/Chip"
-import Footer from "./components/Footer"
-import SmoothScroll from "./components/SmoothScroll"
+import Navbar from "./components/Navbar.tsx"
+import Hero from "./components/Hero.tsx"
+import Highlights2 from "./components/Highlights2.tsx"
+import Model from "./components/Model.tsx"
+import Features from "./components/Features.tsx"
+import Chip from "./components/Chip.tsx"
+import Footer from "./components/Footer.tsx"
+import SmoothScroll from "./components/SmoothScroll.tsx"
 
 
 export default function App() {
