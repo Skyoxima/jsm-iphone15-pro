a9c331aa152aca34a790ae5ce2a436974a5d3b42
Samarth <samarth.tumdi@gmail.com>
2025-04-03 12:35:33 +0530

    Reactivated Highlights Header

--- src/components/Highlights2.tsx
+++ src/components/Highlights2.tsx
@@ -4,25 +4,25 @@ import VideoCarousel2 from "./VideoCarousel2";
 import { animateScrollWithGSAP } from "../utils/animations";
 
 function Highlights2() {
-  // useGSAP(() => {
+  useGSAP(() => {
 
-  //   animateScrollWithGSAP('#title', {
-  //     opacity: 1,
-  //     y: 0
-  //   })
+    animateScrollWithGSAP('#title', {
+      opacity: 1,
+      y: 0
+    })
 
-  //   animateScrollWithGSAP('.link', {
-  //     opacity: 1,
-  //     y: 0,
-  //     stagger: 0.25
-  //   })
+    animateScrollWithGSAP('.link', {
+      opacity: 1,
+      y: 0,
+      stagger: 0.25
+    })
 
-  // }, []);
+  }, []);
 
   return (
     <section id="highlights" className="w-screen h-full common-padding bg-zinc-900 overflow-hidden">
       <div id="section-content-hlts" className="">
-        {/* <div className="mb-12 lg:mb-20 w-full md:flex items-end justify-between screen-max-width">
+        <div className="mb-12 lg:mb-20 w-full md:flex items-end justify-between screen-max-width">
           <h1 id="title" className="section-heading">Get the highlights.</h1>
           <div className="flex flex-wrap items-end gap-5">
             <p className="link">
@@ -34,7 +34,7 @@ function Highlights2() {
               <img src={rightImg} alt='Right' className="ml-2"/>
             </p>
           </div>
-        </div> */}
+        </div>
         <VideoCarousel2 />
       </div>
     </section>
