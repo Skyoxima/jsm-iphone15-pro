8e58c6aa3cebe18ccfde713ecce7673d0ed0f8b6
Samarth <samarth.tumdi@gmail.com>
2025-04-18 01:59:30 +0530

    Gradient animation added to Loader.tsx

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,9 +3,15 @@ import { Html } from "@react-three/drei";
 const Loader = () => {
   return (
     //* This Html wrapper ensures that THREE knows that this is an HTML, not a 3D object to render
-    <Html wrapperClass="w-full h-full -translate-x-1/2 -translate-y-1/2" className="w-full h-full flex-center bg-zinc-800 rounded-2xl">
+    <Html wrapperClass="w-full h-3/4 -translate-x-1/2 -translate-y-1/2" className="w-full h-full flex-center loader-gradient rounded-xl">
       Loading
     </Html>
+    //! Everything below is for testing, hence commented. If using, comment the code above. 
+    // <div className="absolute top-1/2 left-1/2 -translate-1/2 w-[80vw] h-[80vh]">
+    //   <div className="w-full h-full flex-center loader-gradient border-amber-50 border-2 rounded-2xl">
+    //     Loading...
+    //   </div>
+    // </div>
   );
 };
 // wrapperClass goes to the outer div that this Html tag generates and the class goes to the inner, which contains the text
